104b805f969e0f7cd699adc3f93ca174cce4f1b2
Jannis Isensee <Jannis Isensee>
2016-03-29 17:47:50 +0200

    Replaced processStack() with popArguments() which can handle multiple type lists and allows overloading this way.

--- functions.py
+++ functions.py
@@ -1,33 +1,37 @@
 import re
 from errors import InvalidStackContentsException
 
-def processStack(stack, *typeList):
+def popArguments(stack, modeList, arity):
 	"""
-	Checks if the stack contains the given types starting at the top
-	Raises InvalidStackContentsException if the types are not correct or
-	if the amount of given types is greater than the amount of stack elements.
-	Returns the top len(typeList) items from the stack as a list in the order top to bottom.
+	Expects the stack, a modeList and an arity and returns the operands and the corrosponding mode  in the format
+	[mode, operand1, operand2, ...]
+	The modeList has the following format:
+	[{"types" : [int, str, ...], "name" : modeName}, ...]
+	Each list element represents a mode in which the command can work depending on the argument types on the stack
 	"""
-	typeCount = len(typeList)
 	
-	if len(stack) < typeCount:
+	if len(stack) < arity:
 		message = "Expected %s items on the stack, but the stack only contains %s!" % (typeCount, len(stack))
 		raise InvalidStackContentsException(message)
-	else:
-		# Only take the last typeCount items from the stack, ordered top to bottom
-		newStack = stack[-typeCount:][::1]
-		stackTypes = [type(i) for i in newStack]
-		
-		if not all(map(lambda t: t[0] == t[1], zip(stackTypes, typeList))):
-			message = "Excepted the types %s on the stack, but got %s!" % (typeList, stackTypes)
-			raise InvalidStackContentsException(message)
-		else:
-			result = []
-			for i in range(len(typeList)):
-				result.append(stack.pop())
-			
-			return result[0] if len(result) == 1 else result
 
+	# Take the top arity items from the stack
+	topStack = stack[-arity:]
+	topStackTypes = [type(i) for i in topStack]
+
+	# Loop through all the modes and takes the first on that matches the stack types
+	modeName = None
+	for mode in modeList:
+		if all(map(lambda t: t[0] == t[1], zip(topStackTypes, mode["types"]))):
+			modeName = mode["name"]
+			break
+			
+	if modeName:
+		return [modeName] + [stack.pop() for i in range(arity)][::-1]
+	else:
+		typesList = [m["types"] for m in modeList]
+		message =  "Excepted one of the types %s on the stack, but got %s!" % (typesList, topStackTypes)
+		raise InvalidStackContentsException(message)
+	
 """
 The following functions take the stack as paramter, pop the needed arguments and push the result.
 The stack gets mutated in place, so nothing is returned
@@ -35,70 +39,120 @@ A always refers to the top item, B to the item after that, etc.
 """
 			
 def IEval(stack):
-	"""Executes the numeric calculations in A and pushes the result as int"""
 	
-	A = processStack(stack, str)
+	modeList = [{
+			"types" : [str],
+			"name" : "eval"
+		},
+	]
 	
-	# only keep chars for numeric calculations 
-	allowedChars = "1234567890+-/*"
-	result = "".join([c for c in A if c in allowedChars])
-	
-	# remove leading zeros in number literals
-	result = re.sub(r"0+(\d+)", r"\1", result)
-	
-	# Replace float through integer division
-	result = re.sub(r"[^/]/[^/]", "//", result)
-	
-	result = eval(result)
-	
-	stack.append(result)
+	M, A = popArguments(stack, modeList, 1)
+
+	# Executes the numeric calculations in A and pushes the result as int
+	if M == "eval":		
+		# only keep chars for numeric calculations 
+		allowedChars = "1234567890+-/*"
+		result = "".join([c for c in A if c in allowedChars])
+		
+		# remove leading zeros in number literals
+		result = re.sub(r"0+(\d+)", r"\1", result)
+		
+		# Replace float through integer division
+		result = re.sub(r"[^/]/[^/]", "//", result)
+		
+		result = eval(result)
+		
+		stack.append(result)
 
 	
 def IReverse(stack):
-	"""reversed(A)"""
 	
-	A = processStack(stack, str)
+	modeList = [{
+			"types" : [str],
+			"name" : "reverse"
+		},
+	]
 	
-	result = A[::-1]
+	M, A = popArguments(stack, modeList, 1)
 	
-	stack.append(result)
-	
+	# Reverses A
+	if M == "reverse":
+		result = A[::-1]
+		
+		stack.append(result)
+		
 	
 def IStrip(stack):
-	"""A.strip(B)"""
 	
-	A, B = processStack(stack, str, str)
+	modeList = [{
+			"types" : [str, str],
+			"name" : "stripString"
+		}, {
+			"types" : [str, int],
+			"name" : "stripInt"
+		}
+	]
 	
-	result = A.strip(B)
+	M, B, A = popArguments(stack, modeList, 2)
 	
-	stack.append(result)
-	
+	# Remove leading and trailing  As from B
+	if M == "stripString":
+		result = B.strip(A)
+		
+		stack.append(result)
+		
+	# Remove A leading and trailing chars from B
+	elif M == "stripInt":
+		result = B[A : -A]
+		
+		stack.append(result)
 	
 def IWrap(stack):
-	"""Removes all newlines from A"""
 	
-	A = processStack(stack, str)
+	modeList = [{
+			"types" : [str],
+			"name" : "wrap"
+		},
+	]
 	
-	result = "".join(re.split(r"\\n|\\r", A))
+	M, A = popArguments(stack, modeList, 1)
 	
-	stack.append(result)
+	# Remove all newlines from A
+	if M == "wrap":		
+		result = "".join(re.split(r"\\n|\\r", A))
+		
+		stack.append(result)
 	
 	
 def IReplace(stack):
-	"""Replaces all occurences of a regex pattern B with C in A"""
 
-	A = processStack(stack, str, str)
+	modeList = [{
+			"types" : [str, str, str],
+			"name" : "replace"
+		}, 
+	]
 	
-	result = re.sub(B, C, A)
+	M, C, B, A = popArguments(stack, modeList, 3)
 	
-	stack.append(result)
+	# Replaces all occurences of a regex pattern B with A in C
+	if M == "replace":
+		result = re.sub(B, A, C)
+		
+		stack.append(result)
 		
 		
 def IMultiply(stack):
-	"""Repeats every character in A B times"""
 	
-	A, B = processStack(stack, str, int)
+	modeList = [{
+			"types" : [str, int],
+			"name" : "multiply"
+		},
+	]
 	
-	result = "".join([c*B for c in A])
+	M, B, A = popArguments(stack, modeList, 2)
 	
-	stack.append(result)
+	# Repeats every character in B A times
+	if M == "multiply":
+		result = "".join([c*A for c in B])
+		
+		stack.append(result)
