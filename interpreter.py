36dbef79163c57d2f40b1027f3f37802cc752901
DenkerAffe <jannis.isensee@gmail.com>
2016-03-30 16:21:00 +0200

    Outsourced the logic for handling of the commands in interpreter.run() to several function. Indtroduced the types Interger, String and Commands which inherit from Item.

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,79 @@
 from commands import commands
 from errors import CommandNotExistingException, MissingCharacterException, IposException
+from iposTypes import Integer, String, Commands
+
+
+def handleIntegerLiteral(stack, code, index):
+	"""
+	Pushes the integer literal starting at the given index to the stack.
+	Returns the index of the first character after the integer literal.
+	"""
+	x = index
+	while code[x].isdigit():
+		x += 1
+		if x == len(code):
+			break
+	stack.append(Integer(int(code[i : x])))
+	
+	return x
+	
+
+def handleCharLiteral(stack, code, index):
+	"""
+	Pushes the character literal at the given index to the stack.
+	Returns the index of the first character after the character literal
+	"""
+	if index + 1 < len(code):
+		stack.append(String(code[i + 1]))
+		index += 2
+	else:
+		raise MissingCharacterException(index + 1)
+		
+	return index
+	
+
+def handleStringLiteral(stack, code, index):
+	"""
+	Pushes the string literal starting at the given index to the stack.
+	Returns the index of the first character in the code ofter the string literal.
+	"""
+	quoteIndex = code.find("\"", index + 1)
+	if quoteIndex == -1:
+		stack.append(String(code[index + 1 :]))
+		index = len(code)
+	else:
+		stack.append(String(code[index + 1 : quoteIndex]))
+		index = quoteIndex + 1
+		
+	return index
+	
+
+def handleSpace(code, index):
+	"""
+	Returns the index of the first non-whitespace character in the code
+	starting at the given index
+	"""
+	while code[index].isspace():
+		index += 1
+		if index == len(code):
+			break;
+		
+	return index
+	
+
+def handleCommand(stack, code, index):
+	"""
+	Executes the command at the given index in the code.
+	Returns the index of the first character after the command
+	"""
+	if code[index] in commands:
+		commands[code[index]](stack)
+		index += 1
+	else:
+		raise CommandNotExistingException(index, code[index])
+		
+	return index
+
 
 def run(code, stack):
 	""""Runs the code with the given stack"""
@@ -8,52 +82,26 @@ def run(code, stack):
 	i = 0
 	while i < len(code):
 		# If current char is a digit, search the next non-digit char
-		# and push all the digits between to the stack as one integer literal
+		# and push all the digits between them to the stack as one integer literal
 		if code[i].isdigit():
-			x = i
-			while code[x].isdigit():
-				x += 1
-				if x == len(code):
-					break
-			stack.append(int(code[i : x]))
-			# set i on the non-digit char for the next iteration
-			i = x
+			i = handleIntegerLiteral(stack, code, i)
 			
 		# If the current char is a single quote we push the following
 		# character to the stack as  a string literal
 		elif code[i] == "'":
-			if i + 1 < len(code):
-				stack.append(code[i + 1])
-				i += 2
-			else:
-				raise MissingCharacterException(i + 1)
+			i = handleCharLiteral(stack, code, i)
 		
 		# If the current char is a double quote, we search the ending one and push
 		# the string between them to the stack. If there is no ending quote we take everything
 		elif code[i] == "\"":
-			index = code.find("\"", i+1)
-			if index == -1:
-				stack.append(code[i+1 :])
-				i = len(code)
-			else:
-				stack.append(code[i+1 : index])
-				i = index + 1
+			i = handleStringLiteral(stack, code, i)
 		
 		# If current char is a space, find the next non-space char and point i on it
 		elif code[i].isspace():
-			x = i
-			while code[x].isspace():
-				x += 1
-				if x == len(code):
-					break;
-			i = x
+			i = handleSpace(code, i)
 			
 		# Current char is a command
 		else:
-			if code[i] in commands:
-				commands[code[i]](stack)
-				i += 1
-			else:
-				raise CommandNotExistingException(i, code[i])
+			i = handleCommand(stack, code, i)
 	
 	return "".join([str(e) for e in stack])
---
+++ iposTypes.py
@@ -0,0 +1,12 @@
+class Item:
+	def __init__(self, value):
+		self.value = value
+
+class Integer(Item):
+	pass
+		
+class String(Item):
+	pass
+	
+class Commands(Item):
+	pass
